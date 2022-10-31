d10e6f394d061cd7290da666fdedcfc8dcdcfb90
User <User@DESKTOP-ESP2JT1>
2022-10-31 17:02:26 +0200

    Add project files.

---
+++ 08. Pet Shop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace _08._Pet_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dog = 2.50;
+            int cat = 4;
+            int opakDogs = int.Parse(Console.ReadLine());
+            int opakCats = int.Parse(Console.ReadLine());
+            double rezult = opakDogs * 2.5 + opakCats * 4;
+            Console.WriteLine(rezult  +  " lv.");
+
+        }
+    }
+}
